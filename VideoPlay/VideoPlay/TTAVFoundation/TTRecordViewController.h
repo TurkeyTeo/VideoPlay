fd3bcacd3752811940734a2724254669fe4d82fd
TurkeyTeo <790184017@qq.com>
2017-09-25 15:42:57 +0800

    add new fuc

---
+++ VideoPlay/VideoPlay/TTAVFoundation/TTRecordViewController.h
@@ -0,0 +1,13 @@
+//
+//  TTRecordViewController.h
+//  VideoPlay
+//
+//  Created by Thinkive on 2017/9/15.
+//  Copyright © 2017年 Teo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface TTRecordViewController : ViewController
+
+@end
---
+++ VideoPlay/VideoPlay/TTAVFoundation/TTSimplePlayViewController.h
@@ -0,0 +1,16 @@
+//
+//  TTSimplePlayViewController.h
+//  VideoPlay
+//
+//  Created by Thinkive on 2017/9/17.
+//  Copyright © 2017年 Teo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface TTSimplePlayViewController : ViewController
+
+@property (nonatomic, strong) NSURL *url;
+
+
+@end
